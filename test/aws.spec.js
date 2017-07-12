Collaborne/kubernetes-node-aws-sg-updater

commit 0a4227ee9dee2acd01d47e70719c501267befacd

Add basic tests for getNodeAddress
While there:
- fix an issue that getNodeAddress would crash if there are no addresses at all known
- reverse the preference array to make the code easier to follow and allow it to be used as a default parameter

src/aws.js
@@ -90,18 +90,24 @@ export function ruleToIpPermissions(rule, nodeCidrIp) {
 /**
  * Extract address information from the node status.
  *
+ * This tries to find an address based on the given preference for the type.
+ *
  * @package
  * @param {Kubernetes.Node} node
- * @returns
+ * @param {Array<string>} [addressPreference=['ExternalIP', 'InternalIP']]
+ * @returns {Kubernetes.Address} an address, or null if there are no addresses at all known
  */
-export function getNodeAddress(node) {
-	// Find a suitable IP for the node: ExternalIP preferably, otherwise use the InternalIP, or whatever else is there.
-	const addressPreference = [ 'InternalIP', 'ExternalIP' ];
-	const addresses = node.status.addresses.sort((a, b) => addressPreference.indexOf(b.type) - addressPreference.indexOf(a.type));
+export function getNodeAddress(node, addressPreference = [ 'ExternalIP', 'InternalIP' ]) {
+	if (!Array.isArray(node.status.addresses) || node.status.addresses.length === 0) {
+		logger.warn(`Cannot determine ${addressPreference[0]} address of node ${node.metadata.name}, no addresses available`);
+		return null;
+	}
+
+	const addresses = node.status.addresses.sort((a, b) => addressPreference.indexOf(a.type) - addressPreference.indexOf(b.type));
 
 	const nodeAddress = addresses[0];
-	if (nodeAddress.type !== 'ExternalIP') {
-		logger.warn(`Cannot determine ExternalIP address of node ${node.metadata.name}, using ${nodeAddress.type} ${nodeAddress.address}`);
+	if (nodeAddress.type !== addressPreference[0]) {
+		logger.warn(`Cannot determine ${addressPreference[0]} address of node ${node.metadata.name}, using ${nodeAddress.type} ${nodeAddress.address}`);
 	}
 
 	return nodeAddress;

test/aws.spec.js
@@ -1,0 +1,19 @@
+import test from 'tape-catch';
+import * as aws from '../src/aws';
+
+function makeNode(status, name = 'ns/test') {
+	const tmp = name.split('/');
+	return Object.assign({ metadata: { namespace: tmp[0], name: tmp[1] }}, status);
+}
+
+test('getNodeAddress returns empty for missing addresses', assert => {
+	const node = makeNode({ status: { addresses: [ ] }});
+	assert.notOk(aws.getNodeAddress(node));
+	assert.end();
+});
+
+test('getNodeAddress returns preferred type when available', assert => {
+	const node = makeNode({ status: { addresses: [ { type: 'ExternalIP', address: 'external' }, { type: 'InternalIP', address: 'internal' } ] }});
+	assert.deepEqual(aws.getNodeAddress(node), { type: 'ExternalIP', address: 'external' });
+	assert.end();
+});

commit 246400ac54f78da15b79e7ec1202928a807c3922

Fix a bug introduced with 0a4227ee9dee2acd01d47e70719c501267befacd
The refactoring changed the way we used the address preference array to make it more logical
for a caller, but unfortunately that did break the logic. Reverse the array in the code and document
why that is the case.

src/aws.js
@@ -98,16 +98,19 @@ export function ruleToIpPermissions(rule, nodeCidrIp) {
  * @returns {Kubernetes.Address} an address, or null if there are no addresses at all known
  */
 export function getNodeAddress(node, addressPreference = [ 'ExternalIP', 'InternalIP' ]) {
+	const wantedType = addressPreference[0];
 	if (!Array.isArray(node.status.addresses) || node.status.addresses.length === 0) {
-		logger.warn(`Cannot determine ${addressPreference[0]} address of node ${node.metadata.name}, no addresses available`);
+		logger.warn(`Cannot determine ${wantedType} address of node ${node.metadata.name}, no addresses available`);
 		return null;
 	}
 
-	const addresses = node.status.addresses.sort((a, b) => addressPreference.indexOf(a.type) - addressPreference.indexOf(b.type));
+	// Reverse the given preference, so we can sort easily and put irrelevant items to the end.
+	const sortOrder = Array.from(addressPreference).reverse();
+	const addresses = node.status.addresses.sort((a, b) => sortOrder.indexOf(b.type) - sortOrder.indexOf(a.type));
 
 	const nodeAddress = addresses[0];
-	if (nodeAddress.type !== addressPreference[0]) {
-		logger.warn(`Cannot determine ${addressPreference[0]} address of node ${node.metadata.name}, using ${nodeAddress.type} ${nodeAddress.address}`);
+	if (nodeAddress.type !== wantedType) {
+		logger.warn(`Cannot determine ${wantedType} address of node ${node.metadata.name}, using ${nodeAddress.type} ${nodeAddress.address}`);
 	}
 
 	return nodeAddress;

test/aws.spec.js
@@ -7,14 +7,67 @@ function makeNode(status, name = 'ns/test') {
 }
 
 test('getNodeAddress returns empty for missing addresses', assert => {
-	const node = makeNode({ status: { addresses: [ ] }});
+	const node = makeNode({
+		status: {
+			addresses: [
+			]
+		}
+	});
 	assert.notOk(aws.getNodeAddress(node));
 	assert.end();
 });
 
 test('getNodeAddress returns preferred type when available', assert => {
-	const node = makeNode({ status: { addresses: [ { type: 'ExternalIP', address: 'external' }, { type: 'InternalIP', address: 'internal' } ] }});
+	const node = makeNode({
+		status: {
+			addresses: [
+				{ type: 'ExternalIP', address: 'external' },
+				{ type: 'InternalIP', address: 'internal' }
+			]
+		}
+	});
 	assert.deepEqual(aws.getNodeAddress(node), { type: 'ExternalIP', address: 'external' });
+	assert.end();
+});
+
+test('getNodeAddress returns preferred type when unspecific types are available', assert => {
+	const node = makeNode({
+		status: {
+			addresses: [
+				{ type: 'SomethingWeird', address: 'other' },
+				{ type: 'ExternalIP', address: 'external' }
+			]
+		}
+	});
+	
+	assert.deepEqual(aws.getNodeAddress(node), { type: 'ExternalIP', address: 'external' });
+	assert.end();
+});
+
+test('getNodeAddress returns less preferred type when preferred type is not available and unspecific types are available', assert => {
+	const node = makeNode({
+		status: {
+			addresses: [
+				{ type: 'SomethingWeird', address: 'other' },
+				{ type: 'InternalIP', address: 'internal' }
+			]
+		}
+	});
+	
+	assert.deepEqual(aws.getNodeAddress(node), { type: 'InternalIP', address: 'internal' });
+	assert.end();
+});
+
+test('getNodeAddress returns any type when preferred types are not available and unspecific types are available', assert => {
+	const node = makeNode({
+		status: {
+			addresses: [
+				{ type: 'SomethingWeird', address: 'other' },
+			]
+		}
+	});
+	
+	assert.deepEqual(aws.getNodeAddress(node), { type: 'SomethingWeird', address: 'other' });
 	assert.end();
 });
 